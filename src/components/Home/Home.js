cf7f1a75b06c0c50fd4375a609a3c593321a3621
راكينول إسلام <you@example.com>
2022-11-24 20:38:11 +0600

    home done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,14 +3,64 @@ import React from 'react';
 export const Home = () => {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="flex flex-col items-center justify-between lg:flex-row">
-        <div className="mb-10 lg:max-w-lg lg:pr-5 lg:mb-0">
-          <div className="max-w-xl mb-6">
-            <div>
+      <div className='-mt-12'>
+        <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
+          <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
+            <svg
+              className="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block"
+              viewBox="0 0 100 100"
+              fill="currentColor"
+              preserveAspectRatio="none slice"
+            >
+              <path d="M50 0H100L50 100H0L50 0Z" />
+            </svg>
+            <img
+              className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
+              src="https://assets-api.kathmandupost.com/thumb.php?src=https://assets-cdn.kathmandupost.com/uploads/source/news/2020/money/Mobile%20phone%20imports.jpg&w=900&height=601"
+              alt=""
+            />
+          </div>
+          <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
+            <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
               <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
                 Brand new
               </p>
+              <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                Everything you
+                <br className="hidden md:block" />
+                can imagine{" "}
+                <span className="inline-block text-deep-purple-accent-400">
+                  is real
+                </span>
+              </h2>
+              <p className="pr-5 mb-5 text-base text-gray-700 md:text-lg">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae. explicabo.
+              </p>
+              <div className="flex items-center">
+                <a
+                  href="/"
+                  className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                >
+                  Get started
+                </a>
+                <a
+                  href="/"
+                  aria-label=""
+                  className="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700"
+                >
+                  Learn more
+                </a>
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
+      {/* part2 */}
+      <div className="flex flex-col items-center justify-between lg:flex-row mt-20">
+        <div className="mb-10 lg:max-w-lg lg:pr-5 lg:mb-0">
+          <div className="max-w-xl mb-6">
             <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
               The quick, brown fox
               <br className="hidden md:block" />
@@ -25,64 +75,8 @@ export const Home = () => {
               quae. explicabo.
             </p>
           </div>
-          <div className="flex flex-col items-center md:flex-row">
-            <a
-              href="/"
-              className="inline-flex items-center justify-center w-full h-12 px-6 mb-3 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto md:mr-4 md:mb-0 bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-            >
-              <span className="mr-3">Start Shopping</span>
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-4"
-              >
-                <polyline
-                  fill="none"
-                  stroke="currentColor"
-                  strokeMiterlimit="10"
-                  points="4,4 22,4 19,14 4,14 "
-                />
-                <circle
-                  cx="4"
-                  cy="22"
-                  r="2"
-                  strokeLinejoin="miter"
-                  strokeLinecap="square"
-                  stroke="none"
-                  fill="currentColor"
-                />
-                <circle
-                  cx="20"
-                  cy="22"
-                  r="2"
-                  strokeLinejoin="miter"
-                  strokeLinecap="square"
-                  stroke="none"
-                  fill="currentColor"
-                />
-                <polyline
-                  fill="none"
-                  stroke="currentColor"
-                  strokeMiterlimit="10"
-                  points="1,1 4,4 4,14 2,18 23,18 "
-                />
-              </svg>
-            </a>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              Get 15% discount
-            </a>
-          </div>
         </div>
         <div className="relative lg:w-1/2">
-          
           <iframe
             width="560"
             height="315"
@@ -92,7 +86,6 @@ export const Home = () => {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
           ></iframe>
-          
         </div>
       </div>
     </div>
